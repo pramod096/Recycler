5d02453c81cbcb984ab6ed9dbb25e807e2ce96d6
Pramod Reddy Gonegari <pramodreddy096@gmail.com>
2020-08-04 19:23:59 +0530

    Added Recycler View

--- app/src/main/java/pramod/com/recycler/MainActivity.java
+++ app/src/main/java/pramod/com/recycler/MainActivity.java
@@ -1,9 +1,16 @@
 package pramod.com.recycler;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initData() {
-        images = new int[]{R.drawable.aarya,R.drawable.billa, R.drawable.careofkancharapalem,
+        images = new int[]{R.drawable.aarya, R.drawable.billa, R.drawable.careofkancharapalem,
                 R.drawable.darling, R.drawable.gabbar, R.drawable.hello, R.drawable.inception};
 
         movies = new String[]{"Aarya", "Billa", "Care Of Kancharapalem", "Darling", "Gabbar Singh", "Hello", "Inception" };
@@ -26,7 +33,45 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    class RvAdapter extends RecyclerView.Adapter<RvViewHolder> {
+    class RvAdapter extends RecyclerView.Adapter<RvAdapter.RvViewHolder> {
 
+        Context context;
+
+        public RvAdapter(Context context){
+            this.context = context;
+        }
+
+        @NonNull
+        @Override
+        public RvViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(context)
+                    .inflate(R.layout.one_row_layout, false);
+            return new RvViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RvViewHolder holder, int position) {
+            holder.poster.setImageResource(images[position]);
+            holder.mn.setText(movies[position]);
+            holder.an.setText(actors[position]);
+
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return images.length;
+        }
+    }
+
+    public class RvViewHolder extends RecyclerView.ViewHolder {
+        TextView mn,an;
+        ImageView poster;
+        public RvViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mn = itemView.findViewById(R.id.name);
+            an = itemView.findViewById(R.id.actor);
+            poster = itemView.findViewById(R.id.movie_posters);
+        }
     }
 }

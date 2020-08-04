a908c4e8d99e6c67ed968a22dfb1d48d1c9be2cb
Pramod Reddy Gonegari <pramodreddy096@gmail.com>
2020-08-04 19:35:33 +0530

    Added RvAdapter

--- app/src/main/java/pramod/com/recycler/MainActivity.java
+++ app/src/main/java/pramod/com/recycler/MainActivity.java
@@ -2,6 +2,7 @@ package pramod.com.recycler;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
@@ -15,20 +16,27 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private int[] images;
-    private String[] movies,actors;
+    private String[] movies, actors;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         initData();
+        View recyclerView = findViewById(R.id.recyclerview);
+        RvAdapter rva = new RvAdapter(this);
+        recyclerView.setAdapter(rva);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 
     private void initData() {
         images = new int[]{R.drawable.aarya, R.drawable.billa, R.drawable.careofkancharapalem,
                 R.drawable.darling, R.drawable.gabbar, R.drawable.hello, R.drawable.inception};
 
-        movies = new String[]{"Aarya", "Billa", "Care Of Kancharapalem", "Darling", "Gabbar Singh", "Hello", "Inception" };
+        movies = new String[]{"Aarya", "Billa", "Care Of Kancharapalem", "Darling", "Gabbar Singh", "Hello", "Inception"};
         actors = new String[]{"Allu Arjun", "Prabhas", "Karthik", "Prabhas", "Pavan Kalyan", "Akhil", "Leonardo"};
 
     }
@@ -37,15 +45,14 @@ public class MainActivity extends AppCompatActivity {
 
         Context context;
 
-        public RvAdapter(Context context){
+        public RvAdapter(Context context) {
             this.context = context;
         }
 
         @NonNull
         @Override
         public RvViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View v = LayoutInflater.from(context)
-                    .inflate(R.layout.one_row_layout, false);
+            View v = LayoutInflater.from(context).inflate(R.layout.one_row_layout, false);
             return new RvViewHolder(v);
         }
 
@@ -62,16 +69,18 @@ public class MainActivity extends AppCompatActivity {
         public int getItemCount() {
             return images.length;
         }
-    }
 
-    public class RvViewHolder extends RecyclerView.ViewHolder {
-        TextView mn,an;
-        ImageView poster;
-        public RvViewHolder(@NonNull View itemView) {
-            super(itemView);
-            mn = itemView.findViewById(R.id.name);
-            an = itemView.findViewById(R.id.actor);
-            poster = itemView.findViewById(R.id.movie_posters);
+
+        public class RvViewHolder extends RecyclerView.ViewHolder {
+            TextView mn, an;
+            ImageView poster;
+
+            public RvViewHolder(@NonNull View itemView) {
+                super(itemView);
+                mn = itemView.findViewById(R.id.name);
+                an = itemView.findViewById(R.id.actor);
+                poster = itemView.findViewById(R.id.movie_posters);
+            }
         }
     }
 }
